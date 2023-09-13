6688d479576c152128bdaa49975c901c84c8af5f
Nathália Brum <natybrum.97@gmail.com>
2023-09-13 15:32:01 -0300

    Versão com exercícios 4,5,6

---
+++ aula-ts-04/src/index.ts
@@ -0,0 +1,31 @@
+function multiply(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function sum(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+function isEven(num: number): boolean {
+    return num % 2 === 0;
+}
+
+function showResult(result: number): void {
+    if(isEven(result)) {
+        console.log(`The result is ${result} and it's even!`);
+    } else {
+        console.log(`The result is ${result} and it's odd!`);
+    }
+}
+
+(() => {
+    const num1: string | null = prompt("First Number");
+    const num2: string | null = prompt("Second Number");
+
+    const num1Conversao = parseFloat(num1 || "0");
+    const num2Conversao = parseFloat(num2 || "0");
+    
+    let result = sum(num1Conversao,num2Conversao);
+    result += multiply(1,2);
+    showResult(result);
+})();
